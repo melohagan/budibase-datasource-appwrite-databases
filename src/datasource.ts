c4ff976b9564899765358a7e29eece8ffb4386eb
Mel O'Hagan <mel@budibase.com>
2022-10-18 15:44:32 +0100

    Collection CRUD

--- src/datasource.ts
+++ src/datasource.ts
@@ -14,22 +14,37 @@ class CustomIntegration implements IntegrationBase {
     .setKey(config.apiKey)
   }
 
-  async create(query: { databaseId: string; name: string; }) {
+  async create(query: { databaseId: string; collectionId: string; name: string; extra: { [key:string]: string; } }) {
+    if (query.extra.type === "Collections") {
+      return await this.databases.createCollection(query.databaseId, query.collectionId, query.name)
+    }
     return await this.databases.create(query.databaseId, query.name)
   }
 
-  async read(query: { databaseId: string; extra: { [key:string]: string; } }) {
+  async read(query: { databaseId: string; collectionId: string; extra: { [key:string]: string; } }) {
+    if (query.extra.type === "Collections") {
+      if (query.collectionId) {
+        return await this.databases.getCollection(query.databaseId, query.collectionId)
+      }
+      return await this.databases.listCollections(query.databaseId)
+    }
     if (query.databaseId) {
       return await this.databases.get(query.databaseId)
     }
     return await this.databases.list()
   }
 
-  async update(query: { databaseId: string; name: string; }) {
+  async update(query: { databaseId: string; collectionId: string; name: string; extra: { [key:string]: string; } }) {
+    if (query.extra.type === "Collections") {
+      return await this.databases.updateCollection(query.databaseId, query.collectionId, query.name)
+    }
     return await this.databases.update(query.databaseId, query.name)
   }
 
-  async delete(query: { databaseId: string; extra: { [key:string]: string; } }) {
+  async delete(query: { databaseId: string; collectionId: string; extra: { [key:string]: string; } }) {
+    if (query.extra.type === "Collections") {
+      return await this.databases.deleteCollection(query.databaseId, query.collectionId)
+    }
     return await this.databases.delete(query.databaseId)
   }
 }
